f596bb5929314f24ec6693f376809f52da624c04
abhiklaha2000 <abhiklaha2000@gmail.com>
2025-05-08 21:00:45 +0530

    added image

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,7 +5,7 @@ const Portfolio = () => {
     const parallaxRef = useRef(null);
   return (
     <Section className="pt-[12rem] -mt-[5.25rem]" crosses crossesOffset="lg:translate-y-[5.25rem]" customPaddings >
-      <div className="container relative" ref={parallaxRef}>
+      <div className="container relative mb-[5rem]" ref={parallaxRef}>
 
 
       <marquee className="text-4xl text-blue-900">Coming Soon...</marquee>
